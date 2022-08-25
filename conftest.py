09a20667fc3f0e7e75d6bbe14b721899fe3e192b
MakashevaZa <zarina.ma.93@yandex.ru>
2022-08-25 16:36:26 +0300

    Found out how to fix Options

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver import ChromeOptions
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options as ChOptions
+from selenium.webdriver.firefox.options import Options as FiOptions
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
@@ -17,12 +17,12 @@ def browser(request):
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        options = ChromeOptions()
+        options = ChOptions()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        options = Options()
+        options = FiOptions()
         options.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(options=options)
     else:
